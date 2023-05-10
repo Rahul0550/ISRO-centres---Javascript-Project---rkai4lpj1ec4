b7cd28cca72e092f4a753c5008226f2cca0a5f33
Rahul Anand <104300673+Rahul0550@users.noreply.github.com>
2023-05-10 09:36:33 +0530

    changes made

--- App.js
+++ App.js
@@ -1,1 +1,39 @@
 
+// const searchButton = document.getElementById('search-button');
+
+function searchData(){
+    // let searchInput = document.getElementById("searchInput").value;
+    //  alert(searchInput);
+
+     let promiseObject = fetch("https://isro.vercel.app/api/centres");
+
+     promiseObject.then((response) => response.json())
+        .then((response) => console.log(JSON.stringify(response)))
+        // .then(data =>{
+        //     const fileContent = JSON.stringify(data);
+        //     document.getElementById("xmlData").textContent = fileContent;
+        //   })
+        //   .catch(error =>{
+        //     console.error(error);
+        //   })
+        //   show(data);
+    
+}
+
+// function show(data) {
+// 	let tab =
+// 		`<tr>
+// 		<th scope="col">Center Name</th>
+// 		<th scope="col">Place</th>
+// 		<th scope="col">State</th>
+// 		</tr>`;
+
+// 	// Loop to access all rows
+// 	for (let r of data.centres) {
+// 		tab += `<tr class="row">
+//     <td class = "table-data">${r.name}</td>
+// 	<td class = "table-data">${r.Place}</td>
+// 	<td class = "table-data">${r.State}</td>
+// </tr>`;
+// 	}
+// }
---
+++ Untitled-1.dart
@@ -0,0 +1,348 @@
+{
+    "centres": [
+        {
+            "id": 1,
+            "name": "Semi-Conductor Laboratory (SCL)",
+            "Place": "Chandigarh",
+            "State": "Punjab/Haryana"
+        },
+        {
+            "id": 2,
+            "name": "Western RRSC",
+            "Place": "Jodhpur",
+            "State": "Rajasthan"
+        },
+        {
+            "id": 3,
+            "name": "Solar Observatory",
+            "Place": "Udaipur",
+            "State": "Rajasthan"
+        },
+        {
+            "id": 4,
+            "name": "Space Applications Centre (SAC)",
+            "Place": "Ahmedabad",
+            "State": "Gujarat"
+        },
+        {
+            "id": 5,
+            "name": "Physical Research Laboratory (PRL)",
+            "Place": "Ahmedabad",
+            "State": "Gujarat"
+        },
+        {
+            "id": 6,
+            "name": "Development and Educational Communication Unit (DECU)",
+            "Place": "Ahmedabad",
+            "State": "Gujarat"
+        },
+        {
+            "id": 7,
+            "name": "Infrared Observatory",
+            "Place": "Mt.Abu",
+            "State": "Rajasthan"
+        },
+        {
+            "id": 8,
+            "name": "Master Control Facility-B (MCF)",
+            "Place": "Bhopal",
+            "State": "Madhya Pradesh"
+        },
+        {
+            "id": 9,
+            "name": "ISRO Liaison Office",
+            "Place": "Mumbai",
+            "State": "Maharashtra"
+        },
+        {
+            "id": 10,
+            "name": "Indian Deep Space Network (IDSN)",
+            "Place": "Byalalu",
+            "State": "Karnataka"
+        },
+        {
+            "id": 11,
+            "name": "Indian Space Science Data Centre (ISSDC)",
+            "Place": "Byalalu",
+            "State": "Karnataka"
+        },
+        {
+            "id": 12,
+            "name": "Master Control Facility (MCF)",
+            "Place": "Hassan",
+            "State": "Karnataka"
+        },
+        {
+            "id": 13,
+            "name": "Ammonium Perchlorate Experimental Plant",
+            "Place": "Aluva",
+            "State": "Kerala"
+        },
+        {
+            "id": 14,
+            "name": "Space Commission",
+            "Place": "Bengaluru",
+            "State": "Karnataka"
+        },
+        {
+            "id": 15,
+            "name": "Department of Space and ISRO Headquarters",
+            "Place": "Bengaluru",
+            "State": "Karnataka"
+        },
+        {
+            "id": 16,
+            "name": "INSAT Programme Office",
+            "Place": "Bengaluru",
+            "State": "Karnataka"
+        },
+        {
+            "id": 17,
+            "name": "NNRMS Secretariat",
+            "Place": "Bengaluru",
+            "State": "Karnataka"
+        },
+        {
+            "id": 18,
+            "name": "Civil Engineering Programme Office",
+            "Place": "Bengaluru",
+            "State": "Karnataka"
+        },
+        {
+            "id": 19,
+            "name": "Antrix Corporation",
+            "Place": "Bengaluru",
+            "State": "Karnataka"
+        },
+        {
+            "id": 20,
+            "name": "U R Rao Satellite Centre (URSC)",
+            "Place": "Bengaluru",
+            "State": "Karnataka"
+        },
+        {
+            "id": 21,
+            "name": "Laboratory for Electro-Optical Systems (LEOS)",
+            "Place": "Bengaluru",
+            "State": "Karnataka"
+        },
+        {
+            "id": 22,
+            "name": "ISRO Telemetry, Tracking and Command Network (ISTRAC)",
+            "Place": "Bengaluru",
+            "State": "Karnataka"
+        },
+        {
+            "id": 23,
+            "name": "Southern RRSC",
+            "Place": "Bengaluru",
+            "State": "Karnataka"
+        },
+        {
+            "id": 24,
+            "name": "Liquid Propulsion Systems Centre (LPSC)",
+            "Place": "Bengaluru",
+            "State": "Karnataka"
+        },
+        {
+            "id": 25,
+            "name": "Human Space Flight Centre (HSFC)",
+            "Place": "Bengaluru",
+            "State": "Karnataka"
+        },
+        {
+            "id": 26,
+            "name": "New Space India Limited (NSIL)",
+            "Place": "Bengaluru",
+            "State": "Karnataka"
+        },
+        {
+            "id": 27,
+            "name": "New Space India Limited (NSIL)",
+            "Place": "New Delhi",
+            "State": "Delhi"
+        },
+        {
+            "id": 28,
+            "name": "ISRO Branch Office",
+            "Place": "New Delhi",
+            "State": "Delhi"
+        },
+        {
+            "id": 29,
+            "name": "Delhi Earth Station",
+            "Place": "New Delhi",
+            "State": "Delhi"
+        },
+        {
+            "id": 30,
+            "name": "Indian Institute of Remote Sensing (IIRS)",
+            "Place": "Dehradun",
+            "State": "Uttarakand"
+        },
+        {
+            "id": 31,
+            "name": "Centre for Space Science and Technology Education in Asia-Pacific (CSSTEAP)",
+            "Place": "Dehradun",
+            "State": "Uttarakand"
+        },
+        {
+            "id": 32,
+            "name": "ISTRAC Ground Station",
+            "Place": "Lucknow",
+            "State": "Uttar Pradesh"
+        },
+        {
+            "id": 33,
+            "name": "Eastern RRSC",
+            "Place": "Kolkata",
+            "State": "West Bengal"
+        },
+        {
+            "id": 34,
+            "name": "North Eastern Space Application Centre (NE-SAC)",
+            "Place": "Shillong",
+            "State": "Meghalaya"
+        },
+        {
+            "id": 35,
+            "name": "Central RRSC",
+            "Place": "Nagpur",
+            "State": "Maharashtra"
+        },
+        {
+            "id": 36,
+            "name": "National Remote Sensing Centre (NRSC)",
+            "Place": "Hyderabad",
+            "State": "Telangana"
+        },
+        {
+            "id": 37,
+            "name": "National Atmospheric Research Laboratory (NARL)",
+            "Place": "Tirupati",
+            "State": "Andhra Pradesh"
+        },
+        {
+            "id": 38,
+            "name": "Down Range Station",
+            "Place": "Port Blair",
+            "State": "Andaman and Nicobar"
+        },
+        {
+            "id": 39,
+            "name": "Satish Dhawan Space Centre (SDSC), SHAR",
+            "Place": "Sriharikota",
+            "State": "Andhra Pradesh"
+        },
+        {
+            "id": 40,
+            "name": "ISRO Propulsion Complex",
+            "Place": "Mahendragiri",
+            "State": "Tamil Nadu"
+        },
+        {
+            "id": 41,
+            "name": "Vikram Sarabhai Space Centre (VSSC)",
+            "Place": "Thiruvananthapuram",
+            "State": "Kerala"
+        },
+        {
+            "id": 42,
+            "name": "Liquid Propulsion Systems Centre (LPSC)",
+            "Place": "Thiruvananthapuram",
+            "State": "Kerala"
+        },
+        {
+            "id": 43,
+            "name": "ISRO Inertial Systems Unit (IISU)",
+            "Place": "Thiruvananthapuram",
+            "State": "Kerala"
+        },
+        {
+            "id": 44,
+            "name": "Indian Institute of Space Science and Technology (IIST)",
+            "Place": "Thiruvananthapuram",
+            "State": "Kerala"
+        }
+    ]
+}
+
+
+<form>
+  <label for="search-input">Search:</label>
+  <input type="text" id="search-input" name="search-input">
+  <label for="selector">Filter by:</label>
+  <select id="selector" name="selector">
+    <option value="title">Title</option>
+    <option value="author">Author</option>
+    <option value="category">Category</option>
+  </select>
+  <button type="submit">Search</button>
+</form>
+
+
+
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const searchInput = document.querySelector('#search-input').value;
+  const selector = document.querySelector('#selector').value;
+
+  searchBooks(searchInput, selector);
+});
+
+
+function searchBooks(searchInput, selector) {
+  const url = `/books?${selector}=${searchInput}`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      // Do something with the retrieved data
+    })
+    .catch(error => console.error(error));
+}
+
+
+function searchBooks(searchInput, selector) {
+  const url = `/books?${selector}=${searchInput}`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const tableBody = document.querySelector('#results-table tbody');
+      tableBody.innerHTML = '';
+
+      data.forEach(book => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+          <td>${book.title}</td>
+          <td>${book.author}</td>
+          <td>${book.category}</td>
+        `;
+        tableBody.appendChild(row);
+      });
+    })
+    .catch(error => console.error(error));
+}
+
+
+#results-table {
+  border-collapse: collapse;
+  width: 100%;
+}
+
+#results-table th,
+#results-table td {
+  border: 1px solid black;
+  padding: 0.5rem;
+}
+
+#results-table th {
+  background-color: #ddd;
+  font-weight: bold;
+  text-align: left;
+}
