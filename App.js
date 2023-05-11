e823ec40e0d354c45e24c8cdf3e80cafd35872f9
Rahul Anand <104300673+Rahul0550@users.noreply.github.com>
2023-05-11 10:43:23 +0530

    Search filters added

--- App.js
+++ App.js
@@ -1,22 +1,32 @@
 
 function fetchAPIExample() {
     
+    let searchText = document.getElementById("search").value.toLowerCase();
+    let showCity = document.getElementById("btncheck1").checked;
+    let showState = document.getElementById("btncheck2").checked;
+    let showCenter = document.getElementById("btncheck3").checked;
+
     let promiseObject = fetch("https://isro.vercel.app/api/centres");
     promiseObject
       .then((response) => response.json())
       .then((data) =>{
         console.log("What's inside data -> ", data);
     
+        let filteredData = data.centres.filter((result) => {
+          return result.name.toLowerCase().includes(searchText) && 
+            (showCity && result.Place.toLowerCase().includes(searchText)) || 
+            (showState && result.State.toLowerCase().includes(searchText)) || 
+            (showCenter && result.name.toLowerCase().includes(searchText));
+        });
+
         let tableData = "";
-        // data.centres.filter((result) => {
-        //   return result.State == "Gujarat"
-        // })
-        data.centres.map((values) => {
+        
+        filteredData.map((values) => {
           tableData += `<tr>
-                    <td scope="col">${values.name}</td>
-                    <td scope="col">${values.Place}</td>
-                    <td scope="col">${values.State}</td>
-                  <tr>`;
+                          <td scope="col">${values.name}</td>
+                          <td scope="col">${values.Place}</td>
+                          <td scope="col">${values.State}</td>
+                        <tr>`;
         }).join()
         document.querySelector("tbody").innerHTML = tableData;
       })
