76c74b097d51fc8f2fb11224afb44ab6c942b458
Rahul Anand <104300673+Rahul0550@users.noreply.github.com>
2023-05-11 09:06:11 +0530

    added table functionality

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 
 function fetchAPIExample() {
+    
     let promiseObject = fetch("https://isro.vercel.app/api/centres");
     promiseObject
       .then((response) => response.json())
       .then((data) =>{
         console.log("What's inside data -> ", data);
-        const fileContent = JSON.stringify(data);
-        document.getElementById("xmlData").textContent = fileContent;;
+    
         let tableData = "";
-        fileContent.map((values) => {
+        // data.centres.filter((result) => {
+        //   return result.State == "Gujarat"
+        // })
+        data.centres.map((values) => {
           tableData += `<tr>
                     <td scope="col">${values.name}</td>
                     <td scope="col">${values.Place}</td>
                     <td scope="col">${values.State}</td>
                   <tr>`;
-        });
+        }).join()
         document.querySelector("tbody").innerHTML = tableData;
       })
       .catch(error =>{
